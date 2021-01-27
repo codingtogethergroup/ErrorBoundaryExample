fd7b16d28e70ad9faccd2272369b435e1a048bcd
Kate <3837437+katesroad@users.noreply.github.com>
2021-01-26 21:57:33 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,15 @@ import Counter from "./components/Counter";
 import FallbackComponent from "./components/FallbackComponent";
 
 function App() {
-	const [reset, setRest] = React.useState(false);
-	return (
-		<>
-			<button onClick={() => setRest(!reset)}>reset</button>
-			<ErrorBoundary key={reset} FallbackComponent={FallbackComponent}>
-				<Counter />
-			</ErrorBoundary>
-		</>
-	);
+  const [reset, setRest] = React.useState(false);
+  return (
+    <>
+      <button onClick={() => setRest(!reset)}>reset</button>
+      <ErrorBoundary key={reset} FallbackComponent={FallbackComponent}>
+        <Counter />
+      </ErrorBoundary>
+    </>
+  );
 }
 
 export default App;

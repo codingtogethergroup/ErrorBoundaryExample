521d1edced6947ae0abad764feca36d685199fd3
Kate <3837437+katesroad@users.noreply.github.com>
2021-01-26 21:56:22 -0500

    Update index.jsx

--- src/components/FallbackComponent/index.jsx
+++ src/components/FallbackComponent/index.jsx
@@ -1,12 +1,12 @@
 import * as React from "react";
 
 export default function FallbackComponent({ error, info }) {
-	console.log(error, info);
-	return (
-		<>
-			<div>{JSON.stringify(error.message)}</div>
-			<hr />
-			<div>{JSON.stringify(info)}</div>
-		</>
-	);
+  console.log(error, info);
+  return (
+    <>
+      <div>{JSON.stringify(error.message)}</div>
+      <hr />
+      <div>{JSON.stringify(info)}</div>
+    </>
+  );
 }

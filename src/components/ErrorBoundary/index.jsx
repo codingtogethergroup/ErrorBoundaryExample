d8a2d2f9fdbae1edbd592f5f2edf94f45dbed8e4
Kate <3837437+katesroad@users.noreply.github.com>
2021-01-26 21:57:14 -0500

    Update index.jsx

--- src/components/ErrorBoundary/index.jsx
+++ src/components/ErrorBoundary/index.jsx
@@ -1,23 +1,23 @@
 import * as React from "react";
 
 export default class ErrorBoundary extends React.Component {
-	state = { error: null };
+  state = { error: null };
 
-	// This lifecycle is invoked after an error has been thrown by a descendant component.
-	static getDerivedStateFromError(error) {
-		return { error };
-	}
+  // This lifecycle is invoked after an error has been thrown by a descendant component.
+  static getDerivedStateFromError(error) {
+    return { error };
+  }
 
-	// componentDidCatch() is called during the “commit” phase, so side-effects are permitted.d
-	componentDidCatch() {}
+  // componentDidCatch() is called during the “commit” phase, so side-effects are permitted.d
+  componentDidCatch() {}
 
-	render() {
-		const { error, info } = this.state;
-		const { FallbackComponent } = this.props;
-		if (error) {
-			return <FallbackComponent error={error} info={info} />;
-		}
+  render() {
+    const { error, info } = this.state;
+    const { FallbackComponent } = this.props;
+    if (error) {
+      return <FallbackComponent error={error} info={info} />;
+    }
 
-		return this.props.children;
-	}
+    return this.props.children;
+  }
 }
